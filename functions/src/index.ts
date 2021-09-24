42ab6d148878e648ae6abfc06e1f25283482db37
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-09-25 01:37:59 +0900

    a

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,44 +1,34 @@
 import * as functions from 'firebase-functions'
 
 import { adminRef } from './model/admin'
-import {  auth } from './firebaseApp'
+import { auth } from './firebaseApp'
 import { userRef } from './model/user'
 
 const TOKYO = 'asia-northeast1'
 const functionsWithRegion = functions.region(TOKYO)
 
-export const signUpAdmin = functionsWithRegion
-  .https.onCall(async (data: { email: string; password: string }) => {
+export const signUpAdmin = functionsWithRegion.https.onCall(
+  async (data: { email: string; password: string }) => {
     const { email, password } = data
-
-    // const isSGDomain = last(email.split('@')) === 'sonicgarden.jp'
-    // if (!isSGDomain) {
-    //   throw new functions.https.HttpsError("invalid-argument", "")
-    // }
-
     try {
       const authUser = await auth.createUser({ email, password })
-      await adminRef({adminId:authUser.uid}).set({email})
+      await adminRef({ adminId: authUser.uid }).set({ email })
       return authUser.uid
     } catch {
-      throw new functions.https.HttpsError("invalid-argument", "")
+      throw new functions.https.HttpsError('invalid-argument', '')
     }
-  })
+  }
+)
 
-  export const signUpUser = functionsWithRegion
-  .https.onCall(async (data: { email: string; password: string }) => {
+export const signUpUser = functionsWithRegion.https.onCall(
+  async (data: { email: string; password: string }) => {
     const { email, password } = data
-
-    // const isSGDomain = last(email.split('@')) === 'sonicgarden.jp'
-    // if (!isSGDomain) {
-    //   throw new functions.https.HttpsError("invalid-argument", "")
-    // }
-
     try {
       const authUser = await auth.createUser({ email, password })
-      await userRef({userId: authUser.uid}).set({email})
+      await userRef({ userId: authUser.uid }).set({ email })
       return authUser.uid
     } catch {
-      throw new functions.https.HttpsError("invalid-argument", "")
+      throw new functions.https.HttpsError('invalid-argument', '')
     }
-  })
+  }
+)
